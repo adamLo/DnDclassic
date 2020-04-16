55fdc4a41e2d45bfaf9965efeab73ef58c7e30d5
Adam Lovastyik <adam@lovastyik.hu>
2020-04-16 20:08:31 +0200

    Added inventory item type gem
    Added more scenes

--- DnDclassic/Objects/InventoryItem.swift
+++ DnDclassic/Objects/InventoryItem.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum InventoryItemType: String {
     
-    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book
+    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book, gem
     
     var equippable: Bool {
         return [InventoryItemType.weapon, InventoryItemType.armor, InventoryItemType.silverWeapon, InventoryItemType.shield].contains(self)
