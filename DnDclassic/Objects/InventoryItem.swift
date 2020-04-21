da0adb2ade5052f5e4d411d5ac3bd6d204a7819f
Adam Lovastyik <adam@lovastyik.hu>
2020-04-21 14:19:00 +0200

    New inventory item type: cross
    Added more scenes

--- DnDclassic/Objects/InventoryItem.swift
+++ DnDclassic/Objects/InventoryItem.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum InventoryItemType: String {
     
-    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book, gem, cheese, helmet, sword
+    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book, gem, cheese, helmet, sword, cross
     
     var equippable: Bool {
         return [InventoryItemType.weapon, InventoryItemType.armor, InventoryItemType.silverWeapon, InventoryItemType.shield, InventoryItemType.helmet, InventoryItemType.sword].contains(self)
