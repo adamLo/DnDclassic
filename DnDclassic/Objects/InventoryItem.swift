9524600dae282d6d64ce34b72effe1d363e563e3
Adam Lovastyik <adam@lovastyik.hu>
2020-04-23 13:00:31 +0200

    New inventory item type: stake
    More scenes

--- DnDclassic/Objects/InventoryItem.swift
+++ DnDclassic/Objects/InventoryItem.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum InventoryItemType: String {
     
-    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book, gem, cheese, helmet, sword, cross, glove, map, jewelry, arrow
+    case weapon, armor, key, lighting, money, food, potion, silverWeapon, hammer, shield, misc, book, gem, cheese, helmet, sword, cross, glove, map, jewelry, arrow, stake
     
     var equippable: Bool {
         return [InventoryItemType.weapon, InventoryItemType.armor, InventoryItemType.silverWeapon, InventoryItemType.shield, InventoryItemType.helmet, InventoryItemType.sword, InventoryItemType.glove, InventoryItemType.arrow].contains(self)
