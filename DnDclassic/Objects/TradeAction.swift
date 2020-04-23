3143ec0474a4a39ff04d87885b5e39074b21f6a4
Adam Lovastyik <adam@lovastyik.hu>
2020-04-23 20:00:37 +0200

    Trade action wip

---
+++ DnDclassic/Objects/TradeAction.swift
@@ -0,0 +1,22 @@
+//
+//  TradeAction.swift
+//  DnDclassic
+//
+//  Created by Adam Lovastyik on 23/04/2020.
+//  Copyright © 2020 Adam Lovastyik. All rights reserved.
+//
+
+import Foundation
+
+class TradeAction: Action {
+    
+    required init?(json: JSON) {
+        
+        super.init(json: json)
+    }
+    
+    private struct JSONkeys {
+        
+    }
+    
+}
